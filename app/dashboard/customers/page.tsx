a608ffe70b74972135a787964aca38d8891da7d6
Natalie Akam <natalie.akam@gmail.com>
2025-04-27 16:36:17 +0100

    Create db seed route and create example pages

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,12 @@
+import Component from './components/component1';
+
+
+export default function Dashboard() {
+  return (
+    <div className="flex flex-col justify-center items-center gap-2 row-start-2 bg-emerald-700 p-8 w-1/2 h-32">
+      I am a customers page
+      <Component />
+    </div>
+
+  );
+}
